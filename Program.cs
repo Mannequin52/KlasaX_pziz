9d7c65dbeb09096abb4781c1d0fe3524f1302ba8
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2022-12-04 21:18:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremaIspit_KlasaX
+{
+    static class KlasaX
+    {
+        static private int broj;
+
+        static public int Broj { get => broj; set => broj = value; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            KlasaX.Broj = 10;
+
+            Console.WriteLine(Convert.ToString(KlasaX.Broj));
+
+            Console.ReadKey();
+
+        }
+    }
+}
